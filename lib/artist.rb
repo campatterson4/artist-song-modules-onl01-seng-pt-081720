7f85a5fc487765f80b8209742852eca861ff448f
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-23 21:39:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,27 @@
 require 'pry'
 
+
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include PARAMABLE
   attr_accessor :name
-  attr_reader :songs
-
-  @@artists = []
-
-  def initialize
-    @@artists << self
-    @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
-  def self.all
+  attr_reader :songs	
+ @@artists = []	 
+ 
+   def initialize
+   super
+@songs = []
+  end	
+def self.all	  
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
+  end	 
+  
   def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-  def add_songs(songs)
+ @songs << song
+    song.artist = self	
+    
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  end	
 end
